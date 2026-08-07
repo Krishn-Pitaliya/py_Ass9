b73288db2eedaba829183fa34c1f98637d6ce1ca
Krishn Pitaliya <krishnapitaliya@gmail.com>
2026-08-07 12:38:46 +0530

    Update models.py

--- blog/helloworld/models.py
+++ blog/helloworld/models.py
@@ -3,13 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['id']
 
     def __str__(self):
         return self.title
