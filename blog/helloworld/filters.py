f9b3c02b7cd0bc83d0a9b970cced5943e7df1d38
Krishn Pitaliya <krishnapitaliya@gmail.com>
2026-08-07 12:38:09 +0530

    Update filters.py

--- blog/helloworld/filters.py
+++ blog/helloworld/filters.py
@@ -1,9 +1,13 @@
 from django_filters import rest_framework as filters
+
 from .models import Post
 
 
 class PostFilter(filters.FilterSet):
-    created_at = filters.DateFilter(field_name='created_at', lookup_expr='date')
+    created_at = filters.DateFilter(
+        field_name='created_at',
+        lookup_expr='date'
+    )
 
     class Meta:
         model = Post
