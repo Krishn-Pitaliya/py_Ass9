b9eb403196beea238500971b1ad5fbac6f41280f
Krishn Pitaliya <krishnapitaliya@gmail.com>
2026-08-07 12:39:16 +0530

    Refactor permission check for object ownership

--- blog/helloworld/permissions.py
+++ blog/helloworld/permissions.py
@@ -4,4 +4,9 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        current_user = request.user
+
+        if obj.owner == current_user:
+            return True
+
+        return False
