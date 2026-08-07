413c5e4a37429433e0090a4c0329e53c2458793e
Krishn Pitaliya <krishnapitaliya@gmail.com>
2026-08-07 12:43:52 +0530

    Refactor PostListCreateView and PostDetailView methods
    Refactor code for better readability and consistency.

--- blog/helloworld/views.py
+++ blog/helloworld/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
+
 from .models import Post
 from .serializers import PostSerializer
 from .permissions import IsOwner
@@ -8,17 +9,24 @@ from .filters import PostFilter
 
 class PostListCreateView(generics.ListCreateAPIView):
     serializer_class = PostSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
     filterset_class = PostFilter
     search_fields = ['title', 'content']
     ordering_fields = ['id']
-    ordering = ['id']
 
     def get_queryset(self):
-        return Post.objects.filter(owner=self.request.user)
+        user = self.request.user
+        post = Post.objects.filter(owner=user)
+        return post
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        user = self.request.user
+        serializer.save(owner=user)
 
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -26,4 +34,6 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated, IsOwner]
 
     def get_queryset(self):
-        return Post.objects.filter(owner=self.request.user)
+        user = self.request.user
+        post = Post.objects.filter(owner=user)
+        return post
