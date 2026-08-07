4a3c51e5443b3cf4c1cad0b0db7783334d1ee2ae
Krishn Pitaliya <krishnapitaliya@gmail.com>
2026-08-07 12:46:17 +0530

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -123,11 +123,13 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ],
+
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated'
     ],
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
 }
